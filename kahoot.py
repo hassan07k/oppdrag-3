19290a402b4911236feca0597b3e27fb7ebb31ca
Leandro Blanco rivera <leo.b.rivera.07@gmail.com>
2025-03-18 14:13:52 +0100

    flask endringer
    alle flask endringer sånn er linker og sånt i html

--- kahoot.py
+++ kahoot.py
@@ -2,7 +2,17 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.htm")
 
+@app.route("/logginn")
+def logginn():
+    return render_template("logginn.htm")
+
+@app.route("/registrering")
+def registrer():
+    return render_template("registrering.htm")
 
 
 if __name__ == '__main__':
